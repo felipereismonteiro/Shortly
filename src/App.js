65fc89d7adea890e4d7e437ae35834fb37d2ad3b
felipe <felipe.dreis.monteiro@gmail.com>
2022-12-21 15:43:09 -0300

    initializing projetct

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
